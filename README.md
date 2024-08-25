79e3c229a8199494e39e366f4eb0524bafdc2361
Noal Le <109317757+Arussel1@users.noreply.github.com>
2024-08-24 20:46:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node Typescript Template
+This is the template to create a web app with Typescript, Node, Express, PostgreSQL, Pug with passport for authentication. Feel like to modify the template as you wish <br>
---
+++ src/db/pool.ts
@@ -0,0 +1,22 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error('Failed to connect to the database:', err);
+  } else {
+    console.log('Connected to the PostgreSQL database!');
+  }
+});
+
+export default pool;
